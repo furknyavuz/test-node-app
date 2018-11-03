b61abc6294df3245f9ebf64b1aa89502497fb924
Furkan Yavuz <furknyavuz@gmail.com>
2018-11-03 03:58:36 +0300

    #

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
+const express = require('express');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
-const express = require('express');
 
 passport.use(new GitHubStrategy({
         clientID: '50ffe15b2d80064892ce',
         clientSecret: 'd8f30682e806cf5bd72423da2b184f93ee5384f0',
         callbackURL: 'http://127.0.0.1:3000/auth/github/callback'
     },
-
     function (accessToken, refreshToken, profile, done) {
-        console.log(accessToken);
+        console.log('Gathered access token: ' + accessToken);
         return done(null, profile);
     }
 ));
-
-passport.serializeUser(function (user, cb) {
-    cb(null, user);
-});
-
-passport.deserializeUser(function (obj, cb) {
-    cb(null, obj);
-});
 
 const app = express();
 
@@ -32,7 +23,6 @@ app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({extended: true}));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.get('/',
     function (req, res) {
@@ -51,15 +41,9 @@ app.get('/auth/github',
 );
 
 app.get('/auth/github/callback',
-    passport.authenticate('github', {failureRedirect: '/login'}),
+    passport.authenticate('github', {failureRedirect: '/login', session: false }),
     function (req, res) {
-        res.redirect('/profile');
-    }
-);
-
-app.get('/profile',
-    require('connect-ensure-login').ensureLoggedIn(),
-    function (req, res) {
+        console.log('Gathered user: ' + req.user.username);
         res.render('profile', {user: req.user});
     }
 );
